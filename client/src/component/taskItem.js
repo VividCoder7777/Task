f612425c7cffbf64b4993be4323f69bbffff90a8
Raymond <raymond.yu171@gmail.com>
2018-09-11 22:24:57 -0400

    changed to grid

--- client/src/component/taskItem.js
+++ client/src/component/taskItem.js
@@ -7,15 +7,15 @@ export default class TaskItem extends React.Component{
 
 
     render(){
-        let title = this.props.title;
+        let title = this.props.title.toUpperCase();
         let description = this.props.description;
         let taskComplete = this.props.isCompleted;
         console.log(this.props);
         return (
             <div className={taskComplete == false ? 'taskEntry' : 'taskEntryCompleted'}>
                 <span className={taskComplete == false ? 'ongoing' : 'done'}>{taskComplete == false ? 'Ongoing' : 'Completed'}</span>
-                <p id='taskTitle' className='content'><span className='subTitle'>Task Title -</span> {title}</p>
-                <p id='taskDetail' className='content'><span className='subTitle'>Description -</span> {description}</p>
+                <p id='taskTitle' className='content'><span className='subTitle'></span> {title}</p>
+                <p id='taskDetail' className='content'><span className='subTitle'></span> {description}</p>
                 <div id='children'>
                     <button data-complete={this.props.isCompleted} data-id={this.props.id} onClick={this.props.handleComplete}>{this.props.isCompleted == false ? 'Completed' : 'Undo'}</button>
                     <button><Link to={'/task/' + this.props.id + '/edit'}>Edit</Link></button>
