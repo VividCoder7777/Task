7dcb2eec62ef8c21825f1b9edabaf2a07d1b814b
Raymond <raymond.yu171@gmail.com>
2018-09-13 17:06:11 -0400

    dsada

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api', taskRouter);
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use('/api', taskRouter);
 
 
 // catch 404 and forward to error handler
